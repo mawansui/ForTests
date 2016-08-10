7b71b81ccf5785cf3faa46cf83f542091bf272c5
Maxim Shevelev <mdshev7@gmail.com>
2016-08-10 22:17:17 +0300

    make it valid JSON
    make it valid JSON by inserting the curly braces

--- anotherArray.js
+++ anotherArray.js
@@ -1,4 +1,5 @@
 {
+{
 title: 'Заголовок 1',
 subtitle: 'Подзаголовок 1',
 date: '01/02/03',
@@ -18,4 +19,5 @@ subtitle: 'Подзаголовок 3',
 date: '03/02/03',
 image: 'http://sugartin.info/wp-content/uploads/2013/11/logo.png',
 type: 1
+}
 }
