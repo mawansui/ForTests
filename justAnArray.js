14a0dc963498ea333c784ac027197ea2c1311553
Maxim Shevelev <mdshev7@gmail.com>
2016-08-10 22:28:17 +0300

    yet another way of representation
    trying some more. It is just an array now, again.

---
+++ justAnArray.js
@@ -0,0 +1,23 @@
+[
+  {
+    title: 'Заголовок 1',
+    subtitle: 'Подзаголовок 1',
+    date: '01/02/03',
+    image: 'http://sugartin.info/wp-content/uploads/2013/11/logo.png',
+    type: 0
+},
+{
+    title: 'Заголовок 2',
+    subtitle: 'Подзаголовок 2',
+    date: '02/02/03',
+    image: 'http://sugartin.info/wp-content/uploads/2013/11/logo.png',
+    type: 0
+},
+{
+    title: 'Заголовок 3',
+    subtitle: 'Подзаголовок 3',
+    date: '03/02/03',
+    image: 'http://sugartin.info/wp-content/uploads/2013/11/logo.png',
+    type: 1
+}
+];
